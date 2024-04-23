0682f7fc1a75307b8cdacc9d51162984d4832f75
Prathamesh232 <pmvyavhare@gmail.com>
2024-04-23 23:09:11 +0530

    Assignment 2 Q2

---
+++ Assignment 2/Q2/src/DrivingCost/CarPool.java
@@ -0,0 +1,10 @@
+package DrivingCost;
+
+public class CarPool {
+	public static void main(String []args) {
+		drivingCost d = new drivingCost();
+		d.accept();
+		d.display();
+		System.out.println("Thank you for reducing carbon emissions and traffic congestion");
+	}
+}
